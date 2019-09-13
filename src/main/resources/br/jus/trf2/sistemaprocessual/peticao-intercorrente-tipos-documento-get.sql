b59454e6161ba77c85e5660f7d9ded6f8826e687
bpd <bpd@TRF2-025998.trf.net>
2019-09-13 14:16:38 -0300

    alteração da query para retornar tipos de petição de procurador vinculado ao processo

--- src/main/resources/br/jus/trf2/sistemaprocessual/peticao-intercorrente-tipos-documento-get.sql
+++ src/main/resources/br/jus/trf2/sistemaprocessual/peticao-intercorrente-tipos-documento-get.sql
@@ -1,7 +1,8 @@
+SET  @cod_usuario = ? ;
+SET  @num_processo = ? ;
 SELECT
    tipo_peticao_judicial.id_tipo_peticao_judicial AS id,
-   tipo_peticao_judicial.des_peticao AS nome
-
+   tipo_peticao_judicial.des_peticao AS nome 
 FROM
    tipo_peticao_judicial,
    (
@@ -11,7 +12,17 @@ FROM
       FROM
          usuario usu 
       WHERE
-         usu.id_usuario = @cod_usuario
+         usu.id_usuario in 
+         (
+            SELECT
+               id_usuario 
+            FROM
+               usuario,
+               pessoa_identificacao 
+            WHERE
+               usuario.id_pessoa = pessoa_identificacao.id_pessoa 
+               and pessoa_identificacao.ident_principal = @cod_usuario
+         )
    )
    as usu,
    (
@@ -20,8 +31,7 @@ FROM
       FROM
          (
             SELECT
-               u.id_usuario,
-               p.num_processo 
+               u.id_usuario AS codusuario 
             FROM
                usuario u 
                LEFT JOIN
@@ -46,10 +56,89 @@ FROM
                   processo p 
                   ON (ppp.id_processo_parte = p.id_processo) 
             WHERE
-               u.id_usuario = ? 
-               AND p.num_processo = ? 
+               u.id_usuario in 
+               (
+                  SELECT
+                     id_usuario 
+                  FROM
+                     usuario,
+                     pessoa_identificacao 
+                  WHERE
+                     usuario.id_pessoa = pessoa_identificacao.id_pessoa 
+                     and pessoa_identificacao.ident_principal = @cod_usuario
+               )
+               AND p.num_processo = @num_processo 
+            UNION
+            select
+               tabela.id_usuario_procurador as codusuario 
+            from
+               (
+                  SELECT
+                     id_usuario_procurador,
+                     (
+                        SELECT
+                           pn.nome_pessoa 
+                        FROM
+                           usuario u,
+                           pessoa_nome pn 
+                        WHERE
+                           pn.id_pessoa = u.id_pessoa 
+                           and u.id_usuario = id_usuario_procurador
+                     )
+                     as Procurador,
+                     (
+                        SELECT
+                           pn.id_pessoa 
+                        FROM
+                           usuario u,
+                           pessoa_nome pn 
+                        WHERE
+                           pn.id_pessoa = u.id_pessoa 
+                           and u.id_usuario = id_usuario_procurador
+                     )
+                     as Id_pessoa 
+                  FROM
+                     processo_parte_procurador 
+                  WHERE
+                     id_processo_parte IN
+                     (
+                        SELECT
+                           id_processo_parte 
+                        from
+                           processo_parte pp 
+                           inner join
+                              entidade e 
+                              ON (pp.id_pessoa = e.id_pessoa ) 
+                        WHERE
+                           id_processo = 
+                           (
+                              SELECT
+                                 id_processo 
+                              FROM
+                                 processo 
+                              WHERE
+                                 num_processo = @num_processo
+                           )
+                     )
+               )
+               as tabela 
+            where
+               Id_pessoa = 
+               (
+                  SELECT distinct
+                     u.id_pessoa 
+                  FROM
+                     usuario u,
+                     pessoa_nome pnn,
+                     pessoa_identificacao pi 
+                  WHERE
+                     pnn.id_pessoa = u.id_pessoa 
+                     and pi.ident_principal = @cod_usuario 
+                     and pi.id_pessoa = pnn.id_pessoa 
+                     and pi.sin_ativo = 'S'
+               )
          )
-         tabela
+         AS tabela 
    )
    AS usuario_parte 
 WHERE
@@ -63,11 +152,11 @@ WHERE
       AND tipo_peticao_judicial.id_tipo_peticao_judicial IN 
       (
          52,
-         105
+         105 
       )
       AND usu.cod_tipo_usuario = 'A' 
-      AND usuario_parte.parte = false ) # AdvogadoNãoParte 
-      OR  
+      AND usuario_parte.parte = 0 ) 
+      OR 
       (
          tipo_peticao_judicial.id_tipo_peticao_judicial IN 
          (
@@ -135,8 +224,11 @@ WHERE
          )
          AND tipo_peticao_judicial.sin_peticao_procurador = 'S' 
          AND usu.cod_tipo_usuario = 'P' 
-         AND usuario_parte.parte = true 
-      )       
+         AND usuario_parte.parte = 1 
+      )
    )
+GROUP BY
+   id,
+   nome 
 ORDER BY
-   tipo_peticao_judicial.des_peticao ASC
+   tipo_peticao_judicial.des_peticao ASC;
