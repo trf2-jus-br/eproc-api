ceba75d3453adfba4eb10782d18e0fbbc2e36ee8
crivano <renato.crivano@gmail.com>
2020-03-19 12:23:55 -0300

    Restringindo os tipos de usuário que podem se autenticar

--- src/main/resources/br/jus/trf2/sistemaprocessual/autenticar-post.sql
+++ src/main/resources/br/jus/trf2/sistemaprocessual/autenticar-post.sql
@@ -1,24 +1,50 @@
-select
-	p.id_pessoa			codusu
-,	s.password			hash
-,   hf.des_hash_function	hash_function
-,   hp.des_hash_function	hash_persistencia
+SELECT distinct
+   p.id_pessoa codusu,
+   s.password hash,
+   hf.des_hash_function hash_function,
+   hp.des_hash_function hash_persistencia 
 from
-	senha_mysql				s
-,	pessoa					p
-,	pessoa_identificacao	i
-,	pessoa_nome				n
-,	hash_function			hf
-,	hash_function			hp
-where
-	p.id_pessoa				=	s.id_pessoa
-and	p.id_pessoa				=	i.id_pessoa
-and	p.id_pessoa				=	n.id_pessoa
-and	p.seq_nome				=	n.seq_nome_pessoa
-and hf.cod_hash_function	=	s.cod_hash_function
-and hp.cod_hash_function	=	s.cod_hash_persistencia
-and	p.cod_tipo_pessoa		=	'PF'
--- and	s.dth_desativacao		is	null
-and	s.sin_ativo				=	'S'
-and	(i.tipo_identificacao	= 'SIGLA' or i.tipo_identificacao	= 'OAB')
-and	i.ident_principal		=	?;
+   senha_mysql s,
+   usuario u,
+   pessoa p,
+   pessoa_identificacao i,
+   pessoa_nome n,
+   hash_function hf,
+   hash_function hp 
+WHERE
+   u.id_pessoa = p.id_pessoa 
+   and p.id_pessoa = s.id_pessoa 
+   and p.id_pessoa = i.id_pessoa 
+   and p.id_pessoa = n.id_pessoa 
+   and p.seq_nome = n.seq_nome_pessoa 
+   and hf.cod_hash_function = s.cod_hash_function 
+   and hp.cod_hash_function = s.cod_hash_persistencia 
+   and p.cod_tipo_pessoa = 'PF' 	-- and	s.dth_desativacao		is	null
+   and s.sin_ativo = 'S' 
+   AND u.sin_ativo = 'S' 
+   and 
+   (
+      i.tipo_identificacao = 'SIGLA' 
+      or i.tipo_identificacao = 'OAB'
+   )
+   AND 
+   (
+(FIND_IN_SET (u.cod_tipo_usuario, 
+      (
+         SELECT
+            valor 
+         FROM
+            infra_parametro 
+         WHERE
+            nome = 'EPROC_TIPOS_USUARIO_CONSULTA_MNI'
+      )
+) <> 0) 
+      OR u.cod_tipo_usuario IN 
+      (
+         'A',
+         'P',
+         'JUS',
+         'AT'
+      )
+   )
+   and i.ident_principal = ?;
