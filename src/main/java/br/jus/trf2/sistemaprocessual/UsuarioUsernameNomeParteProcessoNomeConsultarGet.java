e6722f6b6b61a44bbc785db28104a098a7cce67c
Alberto Paulino Rodrigues <albertoapr@MacBook-Air-de-Alberto.local>
2021-11-10 19:09:11 -0300

    implementando consulta processual por nome e por documento da parte(cpf e cnpj)

---
+++ src/main/java/br/jus/trf2/sistemaprocessual/UsuarioUsernameNomeParteProcessoNomeConsultarGet.java
@@ -0,0 +1,59 @@
+package br.jus.trf2.sistemaprocessual;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import br.jus.trf2.sistemaprocessual.ISistemaProcessual.IUsuarioUsernameNomeParteProcessoNomeConsultarGet;
+import br.jus.trf2.sistemaprocessual.ISistemaProcessual.Processo;
+
+public class UsuarioUsernameNomeParteProcessoNomeConsultarGet 
+implements IUsuarioUsernameNomeParteProcessoNomeConsultarGet {
+
+	@Override
+	public String getContext() {
+		return "consultar processo pelo nome da parte";
+	}
+
+	@Override
+	public void run(Request req, Response resp, SistemaProcessualContext ctx) throws Exception {
+		
+
+		try (Connection conn = Utils.getConnection(); 
+			PreparedStatement q = conn.prepareStatement(Utils.getSQL("processo-consultar-numero-get"))) {
+			q.setString(1, req.nome);
+			ResultSet rs = q.executeQuery();
+
+			resp.list = new ArrayList<>();
+			while (rs.next()) {
+				Processo p = new Processo();
+				p.numero = rs.getString("numero");
+				p.orgao = EprocServlet.INSTANCE.getProperty("orgao.sigla");
+				p.unidade = rs.getString("unidade");
+				p.localNaUnidade = rs.getString("localnaunidade"); // Apresentar
+																	// isso
+																	// apenas
+																	// para o
+																	// público
+																	// interno
+				p.usuarioautorizado = true; // Esse nós ainda precisamos
+											// descobrir como fazer para
+											// pesquisar?
+				p.segredodejustica = rs.getBoolean("segredodejustica");
+				p.segredodejusticadesistema = rs.getBoolean("segredodejusticadesistema");
+				p.segredodejusticaabsoluto = rs.getBoolean("segredodejusticaabsoluto");
+				p.eletronico = true;
+				p.sentenciado = rs.getBoolean("sentenciado");
+				p.baixado = rs.getBoolean("baixado");
+				p.perdecompetencia = rs.getBoolean("perdecompetencia");
+				p.cdas = rs.getString("cdas");
+				p.dataultimomovimento = Utils.formatarDataHoraMinuto(rs.getTimestamp("dataultimomovimento"));
+				p.autor = rs.getString("autor");
+				p.reu = rs.getString("reu");
+				resp.list.add(p);
+			}
+		}
+
+	}
+}
